55a39808566c5a40be64ccdfbf74e05bbd8acc02
Jose Mejia <jose.mejiab@upb.edu.co>
2026-03-27 14:47:12 -0500

    se modificaron los archivos y actividades de la unidad

---
+++ ejercicio-practico-1.py
@@ -0,0 +1,29 @@
+#ejemplo
+
+import random
+print(random.randint(2,19))
+
+# crear una lñista con 100 numero aleatorios entre 100 y 200
+
+lista = []
+for i in range(100):
+    lista.append(random.randint(100,200))
+    print(lista)
+    mayor = max(lista)
+    print(f"el número más grande de la lista es {mayor}")
+
+# implementar la función max usando un bucle
+
+indice = 0
+may = lista[0]
+while indice < 99:
+    if may < lista[indice+1]
+    may = lista[indice+1]
+indice += 1
+
+print(f"el mayor calculo a mano es: {may}")
+
+
+
+
+
---
+++ listas_met.py
@@ -0,0 +1,71 @@
+from random import randint
+
+lista = []
+
+for i in range(100):
+    dato = randint(0,9)
+    lista.append
+
+
+print(lista)
+ocho = lista.count
+print(f"el número 8 se repite {ocho} veces")
+
+## ejercicio de carros
+
+mayor = max(lista)
+mes = lista.index(mayor)
+####index muestra el indice  del numero mayor
+print(f" se vendieron más autos en {meses[mes]}")
+print(f"se vendieron{mayor} autos")
+
+## 
+mayor = max(lista)
+veces = lista.count
+mes = lista.index(mayor)
+
+print(f" se vendieron más autos en {meses[mes]}")
+print(f"")
+print(f"se vendieron{mayor} autos")
+
+
+print(lista)
+
+mayor = max(lista)
+
+veces = lista.count (mayor)
+
+if veces > 1:
+    pass
+
+
+
+# hacer un ejercicio con insert, remuve y pop
+
+#.insert
+
+lista_vehiculos = ["avion", "moto"]
+
+respuesta = input("ingrese un vehiculo")
+
+lista_vehiculos.insert(0,respuesta)
+
+print(lista_vehiculos)
+
+pass
+
+
+#.remove
+
+lista_vehiculos = ["avion", "moto", "carro"]
+
+lista_vehiculos.remove("avion")
+
+print(lista_vehiculos)
+
+
+# listas de listas
+# traer dos ejemplos con matricez sumando los elementos de todas las filas y encontrar el maximo de toda la matriz
+
+matriz = [{2,4,5}, {5,6,7}, {12,11,9}]
+
